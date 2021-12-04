21d87010518e9c2a27c717cdb2fc4fdc974f4265
VeereshSurya <veereshgaddi94@gmail.com>
2021-12-04 14:16:06 +0530

    Projects first commit with initial Project

---
+++ Prism Crud Application/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Prism_Crud_Application.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace Prism_Crud_Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
